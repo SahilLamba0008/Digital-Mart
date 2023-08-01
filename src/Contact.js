7128a82200f338f335fd727b6cf90a17f8d547ed
SahilLamba0008 <19bcs1922@gmail.com>
2023-08-01 22:06:06 +0530

    Prettier Formatting

--- src/Contact.js
+++ src/Contact.js
@@ -16,7 +16,7 @@ const Contact = () => {
           display: flex;
           flex-direction: column;
           gap: 3rem;
-          
+
           input[type="submit"] {
             cursor: pointer;
             transition: all 0.2s;
@@ -31,23 +31,24 @@ const Contact = () => {
         }
       }
     }
-    .bdr-radius{
+    .bdr-radius {
       border-radius: 7px;
     }
     /* Center the submit button */
     .submit-button-container {
-        display: flex;
-        justify-content: center;
+      display: flex;
+      justify-content: center;
     }
-    .submit-btn{
+    .submit-btn {
       width: 100%;
+      background-color: #223BC9;
     }
   `;
 
   return (
-  <Wrapper>
-    <h2 className="common-heading">Contact Page</h2>
-    <iframe
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+      <iframe
         title="Location-Map"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55939.41040249916!2d76.09840215342349!3d28.802721310182996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3912612b5c6fb30f%3A0x257571e2388eb9eb!2sBhiwani%2C%20Haryana!5e0!3m2!1sen!2sin!4v1689493641911!5m2!1sen!2sin"
         width="100%"
@@ -55,15 +56,16 @@ const Contact = () => {
         style={{ border: 0 }}
         allowFullScreen=""
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade" >
-    </iframe>
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
 
-    <div className="container">
-      <div className="contact-form">
+      <div className="container">
+        <div className="contact-form">
           <form
             action="https://formspree.io/f/xyyqyrla"
             method="POST"
-            className="contact-inputs">
+            className="contact-inputs"
+          >
             <input
               type="text"
               placeholder="username"
@@ -89,15 +91,16 @@ const Contact = () => {
               required
               autoComplete="off"
               placeholder="Enter you message"
-              className="bdr-radius"></textarea>
+              className="bdr-radius"
+            ></textarea>
 
             <div className="submit-button-container">
-              <input type="submit" value="Send" className="submit-btn"/>
+              <input type="submit" value="Send" className="submit-btn" />
             </div>
           </form>
-      </div>  
-    </div>
-  </Wrapper>
+        </div>
+      </div>
+    </Wrapper>
   );
 };
 
--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -15,8 +15,8 @@ import AddToCart from "./components/AddToCart";
 const API = "https://api.pujakaitem.com/api/products";
 
 const SingleProduct = () => {
-
-  const { getSingleProduct, isSingleLoading, singleProduct } = useProductContext();
+  const { getSingleProduct, isSingleLoading, singleProduct } =
+    useProductContext();
   console.log(singleProduct);
 
   const { id } = useParams();
@@ -34,23 +34,24 @@ const SingleProduct = () => {
     image,
   } = singleProduct;
 
-  
   useEffect(() => {
     getSingleProduct(`${API}?id=${id}`);
   }, []);
 
   if (isSingleLoading) {
-    return <div
-    className="page_loading"
-    style={{
-      fontSize: '3.2rem',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <span className="loading_text">Loading...</span>
-  </div>
+    return (
+      <div
+        className="page_loading"
+        style={{
+          fontSize: "3.2rem",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <span className="loading_text">Loading...</span>
+      </div>
+    );
   }
 
   return (
@@ -58,71 +59,70 @@ const SingleProduct = () => {
       <Wrapper>
         <PageNavigation title={name} />
         <div className="content">
-        <Container className="grid grid-two-column">
-          {/* product Images  */}
-          <div className="product_images">
-            <MyImage imgs={image} />
-          </div>
-
-          {/* product dAta  */}
-          <div className="product-data">
-            <h2>{name}</h2>
-            <Star stars={stars} reviews={reviews} />
-
-            <p className="product-data-price">
-              MRP:
-              <del>
-                <FormatPrice price={price + 250000} />
-              </del>
-            </p>
-            <p className="product-data-price product-data-real-price">
-              Deal of the Day: <FormatPrice price={price} />
-            </p>
-            <p>{description}</p>
-            <div className="product-data-warranty">
-              <div className="product-warranty-data">
-                <TbTruckDelivery className="warranty-icon" />
-                <p>Free Delivery</p>
-              </div>
-
-              <div className="product-warranty-data">
-                <TbReplace className="warranty-icon" />
-                <p>30 Days Replacement</p>
-              </div>
-
-              <div className="product-warranty-data">
-                <TbTruckDelivery className="warranty-icon" />
-                <p>Thapa Delivered </p>
-              </div>
-
-              <div className="product-warranty-data">
-                <MdSecurity className="warranty-icon" />
-                <p>2 Year Warranty </p>
-              </div>
+          <Container className="grid grid-two-column">
+            {/* product Images  */}
+            <div className="product_images">
+              <MyImage imgs={image} />
             </div>
 
-            <div className="product-data-info">
-              <p>
-                Available:
-                <span> {stock > 0 ? "In Stock" : "Not Available"}</span>
+            {/* product dAta  */}
+            <div className="product-data">
+              <h2>{name}</h2>
+              <Star stars={stars} reviews={reviews} />
+
+              <p className="product-data-price">
+                MRP:
+                <del>
+                  <FormatPrice price={price + 250000} />
+                </del>
               </p>
-              <p>
-                Brand :<span> {company} </span>
+              <p className="product-data-price product-data-real-price">
+                Deal of the Day: <FormatPrice price={price} />
               </p>
+              <p>{description}</p>
+              <div className="product-data-warranty">
+                <div className="product-warranty-data">
+                  <TbTruckDelivery className="warranty-icon" />
+                  <p>Free Delivery</p>
+                </div>
+
+                <div className="product-warranty-data">
+                  <TbReplace className="warranty-icon" />
+                  <p>30 Days Replacement</p>
+                </div>
+
+                <div className="product-warranty-data">
+                  <TbTruckDelivery className="warranty-icon" />
+                  <p>Thapa Delivered </p>
+                </div>
+
+                <div className="product-warranty-data">
+                  <MdSecurity className="warranty-icon" />
+                  <p>2 Year Warranty </p>
+                </div>
+              </div>
+
+              <div className="product-data-info">
+                <p>
+                  Available:
+                  <span> {stock > 0 ? "In Stock" : "Not Available"}</span>
+                </p>
+                <p>
+                  Brand :<span> {company} </span>
+                </p>
+              </div>
+              <hr />
+              {stock > 0 && <AddToCart product={singleProduct} />}
             </div>
-            <hr/>
-            {stock > 0 && <AddToCart product={singleProduct} />}
-          </div>
-        </Container>
+          </Container>
         </div>
       </Wrapper>
     </>
-  )
+  );
 };
 
-
 const Wrapper = styled.section`
-  .content{
+  .content {
     margin-top: 5rem;
   }
   .container {
