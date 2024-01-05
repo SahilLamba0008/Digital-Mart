41f423304f17be374476bedcd05c43766ec86cbb
SahilLamba0008 <19bcs1922@gmail.com>
2024-01-05 23:15:06 +0530

    .

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { FaCheck } from "react-icons/fa";
 import CartAmountToggle from "./CartAmountToggle";
 import { NavLink } from "react-router-dom";
-import { Button } from "../styles/Button";
 
 const AddToCart = ({ product }) => {
   const { colors, stock } = product;
